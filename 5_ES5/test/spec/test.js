29a5f0840179f603904e58f54e9b459e442951c6
Oliver Ochs <oliver.ochs@holisticon.de>
2014-05-20 10:50:54 +0200

    Test inheritance (reminder)

--- 5_ES5/test/spec/test.js
+++ 5_ES5/test/spec/test.js
@@ -18,6 +18,41 @@
                 myTask.subject = 'My Task';
                 assert(myTask.subject === 'My Task');
             });
+            it('should be possible to overwrite values', function () {
+                var myTask = Object.create(task);
+                myTask.subject = 'My Task';
+                assert(myTask.subject === 'My Task');
+
+                var myOtherTask = Object.create(myTask);
+                assert(myOtherTask.subject === 'My Task');
+
+                myOtherTask.subject = 'My other Task';
+                assert(myOtherTask.subject === 'My other Task');
+                assert(myTask.subject === 'My Task');
+
+            });
+        });
+        describe ('A Reminder', function() {
+            it('should provide a Reminder object', function () {
+                var myReminder = Object.create(reminder);
+                assert(myReminder);
+            });
+            it('should provide a subject on a Reminder object', function () {
+                var myReminder = Object.create(reminder);
+                assert(myReminder.subject === '');
+            });
+            it('should provide a changeable subject on a Reminder object', function () {
+                var myReminder = Object.create(reminder);
+                myReminder.subject = 'My Reminder';
+                assert(myReminder.subject === 'My Reminder');
+            });
+            it('should provide a changeable date on a Reminder object', function () {
+                var myReminder = Object.create(reminder);
+                var now = Date.now();
+                assert(myReminder.date !== now);
+                myReminder.date = now;
+                assert(myReminder.date === now);
+            });
         });
         describe ('A TaskList', function(){
             it('should provide a taskList', function(){
